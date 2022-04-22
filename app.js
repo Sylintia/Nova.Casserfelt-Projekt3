8af2315911417555b2c68007cc3b7c10744adeb7
Nova Casserfelt <casserfelt.nova@elev.ga.ntig.se>
2022-04-22 13:52:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let app = Vue.createApp({
+    data() {
+        return {
+            title: "asmdaos",
+            enableInput: false,
+            brands: ["JYPE", "SME", "YGE", "HYBE"]
+        }
+    }
+});
+
+app.mount("#app-root");
